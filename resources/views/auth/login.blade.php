04acad791af89bffa4af7a87918ff49b27964384
satriyoaji <satriyoaji27@gmail.com>
2020-06-03 22:10:48 +0700

    Add UI theme on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,59 +1,69 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <title>{{ config('gate.name', 'Laravel') }} | Dashboard</title>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+    <link href="{{URL::asset('theme/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('theme/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+
+    <!-- Toastr style -->
+    <link href="{{URL::asset('theme/css/plugins/toastr/toastr.min.css')}}" rel="stylesheet">
+
+    <!-- Gritter -->
+    <link href="{{URL::asset('theme/js/plugins/gritter/jquery.gritter.css')}}" rel="stylesheet">
+
+    <link href="{{URL::asset('theme/css/animate.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('theme/css/style.css')}}" rel="stylesheet">
+
+
+</head>
+<body class="gray-bg">
+
+    <div class="loginColumns animated fadeInDown">
+        <div class="row">
+
+            <div class="col-md-6">
+                <div class="card-header text-center">{{ __('Login Gate') }}</div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="ibox-content">
+                    <form class="m-t" role="form" method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary block full-width m-b">
                                     {{ __('Login') }}
                                 </button>
 
@@ -64,10 +74,26 @@
                                 @endif
                             </div>
                         </div>
+
                     </form>
+
                 </div>
             </div>
         </div>
+
+        <hr/>
+        <div class="row">
+            <div class="col-md-6">
+                <strong>Copyright</strong> Smartaircraft
+            </div>
+            <div class="col-md-6 text-right">
+                <small>© {{date('Y')}}</small>
+            </div>
+        </div>
+
     </div>
-</div>
-@endsection
+
+</body>
+
+</html>
+
