5522bc8de08ddf7da4d5db3cae5207cd7cb3452d
ibnuprtma <pibnu86@gmail.com>
2020-06-07 18:13:19 +0700

    disabled route register

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes(['register'=>false]);
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::view('dashboard', 'dashboard')->name('dashboard');
