8f93273207b77d23f172aad014b39436c5775b7b
ibnuprtma <pibnu86@gmail.com>
2020-06-07 18:18:15 +0700

    refactory templating

---
+++ Modules/Gate/Resources/views/pages/company/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+
+@section('page-heading')
+
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        @php
+            $name = "Company";   
+        @endphp
+
+        <h2>{{ $name }}</h2>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="{{route('dashboard')}}">Home</a>
+            </li>
+            <li class="breadcrumb-item active">
+                <a>{{ $name }}</a>
+            </li>
+        </ol>
+    </div>
+</div>
+
+@endsection
+
+@section('content')
+    <h1>Hello Company</h1>
+
+    <p>
+        This view is loaded from module: {!! config('gate.name') !!}
+    </p>
+@endsection
--- Modules/Gate/Resources/views/user/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('gate::layouts.master')
-
-@section('content')
-    <h1>Hello User</h1>
-
-    <p>
-        This view is loaded from module: {!! config('gate.name') !!}
-    </p>
-@endsection
