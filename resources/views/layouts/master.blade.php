39e4f8957afbce5d854c1e098894d19c3e5c8f2f
satriyoaji <satriyoaji27@gmail.com>
2020-06-11 08:41:36 +0700

    add comments

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
 
         @include('layouts.includes._header-script')
 
+        {{-- jika ada tambahan custom script pada header --}}
         @stack('header-scripts')
     </head>
     <body>
@@ -24,11 +25,12 @@
                 </nav>
             </div>
 
+            {{-- membaca request http sesuai route --}}
             @if (Request::is('dashboard'))
                 @yield('content')
             @else
                 @yield('page-heading')
-                
+
                 <div class="wrapper wrapper-content animated fadeInRight">
                     @yield('content')
                 </div>
@@ -47,6 +49,7 @@
 
     @include('layouts.includes._footer-script')
 
+    {{-- jika ada tambahan custom script pada footer --}}
     @stack('footer-scripts')
     </body>
 </html>
