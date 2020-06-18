46f9ed81b39b6e46d077910e0b88ff486fcb5d8d
satriyoaji <satriyoaji27@gmail.com>
2020-06-19 01:20:52 +0700

    make components on App

---
+++ resources/views/components/breadcrumb.blade.php
@@ -0,0 +1,11 @@
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2>{{ $name }}</h2>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="/gate">Home</a>
+            </li>
+            {{$slot}}
+        </ol>
+    </div>
+</div>
