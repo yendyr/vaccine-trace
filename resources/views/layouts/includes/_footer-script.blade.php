8ace9cc626a9f3ec84180192bd55453ab24d027d
satriyoaji <satriyoaji27@gmail.com>
2020-06-23 11:10:42 +0700

    adding DataTable foo environment (CSS & JS)

--- resources/views/layouts/includes/_footer-script.blade.php
+++ resources/views/layouts/includes/_footer-script.blade.php
@@ -8,15 +8,15 @@
     <script src="{{URL::asset('theme/js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
 
     <!-- Flot -->
-    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.js')}}"></script>
-    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>
-    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.spline.js')}}"></script>
-    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.resize.js')}}"></script>
-    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.pie.js')}}"></script>
+{{--    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.js')}}"></script>--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.spline.js')}}"></script>--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.resize.js')}}"></script>--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/flot/jquery.flot.pie.js')}}"></script>--}}
 
-    <!-- Peity -->
-    <script src="{{URL::asset('theme/js/plugins/peity/jquery.peity.min.js')}}"></script>
-    <script src="{{URL::asset('theme/js/demo/peity-demo.js')}}"></script>
+{{--    <!-- Peity -->--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/peity/jquery.peity.min.js')}}"></script>--}}
+{{--    <script src="{{URL::asset('theme/js/demo/peity-demo.js')}}"></script>--}}
 
     <!-- Custom and plugin javascript -->
     <script src="{{URL::asset('theme/js/inspinia.js')}}"></script>
@@ -25,24 +25,34 @@
     <!-- jQuery UI -->
     <script src="{{URL::asset('theme/js/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 
-    <!-- GITTER -->
-    <script src="{{URL::asset('theme/js/plugins/gritter/jquery.gritter.min.js')}}"></script>
+{{--    <!-- GITTER -->--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/gritter/jquery.gritter.min.js')}}"></script>--}}
 
-    <!-- Sparkline -->
-    <script src="{{URL::asset('theme/js/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+{{--    <!-- Sparkline -->--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/sparkline/jquery.sparkline.min.js')}}"></script>--}}
 
-    <!-- Sparkline demo data  -->
-    <script src="{{URL::asset('theme/js/demo/sparkline-demo.js')}}"></script>
+{{--    <!-- Sparkline demo data  -->--}}
+{{--    <script src="{{URL::asset('theme/js/demo/sparkline-demo.js')}}"></script>--}}
 
-    <!-- ChartJS-->
-    <script src="{{URL::asset('theme/js/plugins/chartJs/Chart.min.js')}}"></script>
+{{--    <!-- ChartJS-->--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/chartJs/Chart.min.js')}}"></script>--}}
 
-    <!-- Toastr -->
-    <script src="{{URL::asset('theme/js/plugins/toastr/toastr.min.js')}}"></script>
+{{--    <!-- Toastr -->--}}
+{{--    <script src="{{URL::asset('theme/js/plugins/toastr/toastr.min.js')}}"></script>--}}
 
+
+    <!-- Data Foo Table-->
+    <script src="{{URL::asset('theme/js/plugins/footable/footable.all.min.js')}}"></script>
+    <script>
+        $(document).ready(function() {
+
+            $('.footable').footable();
+            $('.footable2').footable();
+
+        });
+    </script>
 
     <script>
-
         $(window).bind("scroll", function () {
             let toast = $('.toast');
             toast.css("top", window.pageYOffset + 20);
--- resources/views/layouts/includes/_header-script.blade.php
+++ resources/views/layouts/includes/_header-script.blade.php
@@ -5,14 +5,17 @@
 <link href="{{URL::asset('theme/css/plugins/iCheck/custom.css')}}" rel="stylesheet">
 <link href="{{URL::asset('theme/css/plugins/steps/jquery.steps.css')}}" rel="stylesheet">
 
-<!-- Toastr style -->
-<link href="{{URL::asset('theme/css/plugins/toastr/toastr.min.css')}}" rel="stylesheet">
+{{--<!-- Toastr style -->--}}
+{{--<link href="{{URL::asset('theme/css/plugins/toastr/toastr.min.css')}}" rel="stylesheet">--}}
 
-<!-- Gritter -->
-<link href="{{URL::asset('theme/js/plugins/gritter/jquery.gritter.css')}}" rel="stylesheet">
+{{--<!-- Gritter -->--}}
+{{--<link href="{{URL::asset('theme/js/plugins/gritter/jquery.gritter.css')}}" rel="stylesheet">--}}
 
 <link href="{{URL::asset('theme/css/animate.css')}}" rel="stylesheet">
 <link href="{{URL::asset('theme/css/style.css')}}" rel="stylesheet">
+
+<!-- Data Foo Table -->
+<link href="{{URL::asset('theme/css/plugins/footable/footable.core.css')}}" rel="stylesheet">
 
 <!-- Custom -->
 <link href="{{URL::asset('assets/custom.css')}}" rel="stylesheet">
