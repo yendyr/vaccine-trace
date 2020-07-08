842011bdf1fa2889abeb4bcb8c20c5674edf273a
satriyoaji <satriyoaji27@gmail.com>
2020-07-08 22:54:25 +0700

    update routes for CRUD using ajax and modal bootstrap

--- Modules/Gate/Routes/web.php
+++ Modules/Gate/Routes/web.php
@@ -13,11 +13,21 @@
 
 Route::name('gate.')->group(function () {
     Route::prefix('gate')->group(function() {
+        Route::get('/user/select2/role', 'UserController@select2Role')->name('user.select2.role');
+        Route::get('/user/select2/company', 'UserController@select2Company')->name('user.select2.company');
         Route::resource('/user', 'UserController');
+
         Route::resource('/company', 'CompanyController');
+
         Route::resource('/role', 'RoleController');
+
+        Route::get('/role-menu/select2/role', 'RoleMenuController@select2Role')->name('role-menu.select2.role');
         Route::resource('/role-menu', 'RoleMenuController');
+
+        Route::get('/role-menu/datatable/{role}', 'RoleMenuController@fetch')->name('role-menu.datatable');
+
         Route::get('/', 'GateController@index');
-        //Route::view('/test', 'gate::pages.test.index')->name('test.index');
+
+        Route::view('/test', 'gate::pages.test.index')->name('test.index');
     });
 });
