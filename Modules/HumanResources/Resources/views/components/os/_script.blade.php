62a126e9de140ee0d12ff15b6beabb1538d2411b
satriyoaji <satriyoaji27@gmail.com>
2020-07-27 09:10:06 +0700

    modify tree grid view data

--- Modules/HumanResources/Resources/views/components/os/_script.blade.php
+++ Modules/HumanResources/Resources/views/components/os/_script.blade.php
@@ -5,3 +5,148 @@
     <script src="https://cdn.syncfusion.com/ej2/dist/ej2.min.js" type="text/javascript"></script>
 
 @endpush
+
+@push('footer-scripts')
+    <script>
+        {{--$(document).ready(function () {--}}
+        {{--    var ostId;--}}
+
+        {{--    var table = $('#ost-table').DataTable({--}}
+        {{--        processing: true,--}}
+        {{--        serverSide: true,--}}
+        {{--        ajax: {--}}
+        {{--            url: "{{ route('hr.ost.index')}}",--}}
+        {{--        },--}}
+        {{--        columns: [--}}
+        {{--            { data: 'titlecode', name: 'titlecode' },--}}
+        {{--            { data: 'jobtitle', name: 'jobtitle' },--}}
+        {{--            { data: 'rptorg.name', name: 'rptorg', defaultContent: "<p class='text-muted'>none</p>" },--}}
+        {{--            { data: 'rpttitle.title', name: 'rpttitle', defaultContent: "<p class='text-muted'>none</p>" },--}}
+        {{--            { data: 'status', name: 'status' },--}}
+        {{--            { data: 'action', name: 'action', orderable: false },--}}
+        {{--            { data: 'orgcode', name: 'orgcode', visible: false },--}}
+        {{--        ]--}}
+        {{--    });--}}
+
+        {{--    $('.select2_orgparent').select2({--}}
+        {{--        placeholder: 'choose here',--}}
+        {{--        ajax: {--}}
+        {{--            url: "{{route('hr.os.select2.orgcode')}}",--}}
+        {{--            dataType: 'json',--}}
+        {{--        },--}}
+        {{--        dropdownParent: $('#osForm')--}}
+        {{--    });--}}
+        {{--    $('#createOST').click(function () {--}}
+        {{--        $('#saveBtn').val("create-os");--}}
+        {{--        $('#ostForm').trigger("reset");--}}
+        {{--        $('#modalTitle').html("Add New Organization Structure data");--}}
+        {{--        $('[class^="invalid-feedback-"]').html('');  //delete html all alert with pre-string invalid-feedback--}}
+        {{--        $('#ostModal').modal('show');--}}
+        {{--        $('#ostForm').attr('action', '/hr/ost');--}}
+        {{--        $("input[value='patch']").remove();--}}
+        {{--    });--}}
+
+        {{--    table.on('click', '.editBtn', function () {--}}
+        {{--        $('#ostForm').trigger("reset");--}}
+        {{--        $('#modalTitle').html("Update Organization Structure data");--}}
+        {{--        ostId= $(this).val();--}}
+        {{--        let tr = $(this).closest('tr');--}}
+        {{--        let data = table.row(tr).data();--}}
+
+        {{--        $('<input>').attr({--}}
+        {{--            type: 'hidden',--}}
+        {{--            name: '_method',--}}
+        {{--            value: 'patch'--}}
+        {{--        }).prependTo('#ostForm');--}}
+
+        {{--        $('#forgcode').empty();--}}
+        {{--        $('#forgcode').append('<option value="' + data.orgcode.code + '" selected>' + data.orgcode.code + ' - ' + data.orgcode.name + '</option>');--}}
+
+        {{--        $('#ftitlecode').find('option').removeAttr('selected');--}}
+        {{--        if (data.titlecode == 1){--}}
+        {{--            $('#ftitlecode').find('option[value="1"]').attr('selected', '');--}}
+        {{--        }else if (data.titlecode == 2){--}}
+        {{--            $('#ftitlecode').find('option[value="2"]').attr('selected', '');--}}
+        {{--        }else if (data.titlecode == 3){--}}
+        {{--            $('#ftitlecode').find('option[value="3"]').attr('selected', '');--}}
+        {{--        }else if (data.titlecode == 4){--}}
+        {{--            $('#ftitlecode').find('option[value="4"]').attr('selected', '');--}}
+        {{--        }else if (data.titlecode == 5){--}}
+        {{--            $('#ftitlecode').find('option[value="5"]').attr('selected', '');--}}
+        {{--        }--}}
+
+        {{--        $('#fjobtitle').val(data.jobtitle);--}}
+
+        {{--        if (data.rptorg == null){--}}
+        {{--            $('#frptorg').append('<option value="' + 0 + '" selected>none</option>');--}}
+        {{--        } else{--}}
+        {{--            $('#frptorg').append('<option value="' + data.rptorg.code + '" selected>' + data.rptorg.code + ' - ' + data.rptorg.name + '</option>');--}}
+        {{--        }--}}
+
+        {{--        if (data.rpttitle == null){--}}
+        {{--            $('#frpttitle').append('<option value="' + 0 + '" selected>none</option>');--}}
+        {{--        } else{--}}
+        {{--            $('#frpttitle').append('<option value="' + data.rpttitle.code + '" selected>' + data.rpttitle.title + '</option>');--}}
+        {{--        }--}}
+
+        {{--        $('#fstatus').find('option').removeAttr('selected');--}}
+        {{--        if (data.status == '<p class="text-success">Active</p>'){--}}
+        {{--            $('#fstatus').find('option[value="1"]').attr('selected', '');--}}
+        {{--        }else{--}}
+        {{--            $('#fstatus').find('option[value="0"]').attr('selected', '');--}}
+        {{--        }--}}
+
+        {{--        $('#saveBtn').val("edit-ost");--}}
+        {{--        $('#ostForm').attr('action', '/hr/ost/' + data.id);--}}
+
+        {{--        $('[class^="invalid-feedback-"]').html('');  //delete html all alert with pre-string invalid-feedback--}}
+        {{--        $('#ostModal').modal('show');--}}
+        {{--    });--}}
+
+        {{--    $('#ostForm').on('submit', function (event) {--}}
+        {{--        event.preventDefault();--}}
+        {{--        let url_action = $(this).attr('action');--}}
+        {{--        $.ajax({--}}
+        {{--            headers: {--}}
+        {{--                "X-CSRF-TOKEN": $(--}}
+        {{--                    'meta[name="csrf-token"]'--}}
+        {{--                ).attr("content")--}}
+        {{--            },--}}
+        {{--            url: url_action,--}}
+        {{--            method: "POST",--}}
+        {{--            data: $(this).serialize(),--}}
+        {{--            dataType: 'json',--}}
+        {{--            beforeSend:function(){--}}
+        {{--                $('#saveBtn').html('<strong>Saving...</strong>');--}}
+        {{--                $('#saveBtn').prop('disabled', true);--}}
+        {{--            },--}}
+        {{--            success:function(data){--}}
+        {{--                if (data.success) {--}}
+        {{--                    $('#form_result').attr('class', 'alert alert-success alert-dismissable fade show font-weight-bold');--}}
+        {{--                    $('#form_result').html(data.success +--}}
+        {{--                        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">\n' +--}}
+        {{--                        '    <span aria-hidden="true">&times;</span>\n' +--}}
+        {{--                        '  </button>');--}}
+        {{--                }--}}
+        {{--                $('#ostModal').modal('hide');--}}
+        {{--                $('#ost-table').DataTable().ajax.reload();--}}
+        {{--            },--}}
+        {{--            error:function(data){--}}
+        {{--                let errors = data.responseJSON.errors;--}}
+        {{--                if (errors) {--}}
+        {{--                    $.each(errors, function (index, value) {--}}
+        {{--                        $('div.invalid-feedback-'+index).html(value);--}}
+        {{--                    })--}}
+        {{--                }--}}
+        {{--            },--}}
+        {{--            complete:function(){--}}
+        {{--                $('#saveBtn').prop('disabled', false);--}}
+        {{--                console.log($('#saveBtn').attr('value'));--}}
+        {{--                $('#saveBtn').html('<strong>Save Changes</strong>');--}}
+        {{--            }--}}
+        {{--        });--}}
+        {{--    });--}}
+        {{--});--}}
+
+    </script>
+@endpush
--- Modules/HumanResources/Resources/views/pages/os-ost/index.blade.php
+++ Modules/HumanResources/Resources/views/pages/os-ost/index.blade.php
@@ -119,7 +119,7 @@
         </div>
     </div>
 
-{{--    @can('viewAny', \Modules\HumanResources\Entities\OrganizationStructureTitle::class)--}}
+{{--    @can('viewAny', \Modules\HumanResources\Entities\OrganizationStructure::class)--}}
         @push('footer-scripts')
             <script>
                 var ele = document.getElementById('container');
@@ -128,12 +128,8 @@
                 }
             </script>
             <script type="text/javascript">
-                ej.treegrid.TreeGrid.Inject(ej.treegrid.Edit, ej.treegrid.Toolbar, ej.treegrid.Sort, ej.treegrid.Filter, ej.data.WebApiAdaptor);
-                {{--var data = new ej.data.DataManager({--}}
-                {{--    url: '{{route("hr.os.index")}}',--}}
-                {{--    adaptor: new ej.data.WebApiAdaptor(),--}}
-                {{--    crossDomain: true--}}
-                {{--});--}}
+                ej.treegrid.TreeGrid.Inject(ej.treegrid.Edit, ej.treegrid.Toolbar, ej.treegrid.Sort, ej.treegrid.Filter);
+
                 var treeGridObj = new ej.treegrid.TreeGrid({
                     dataSource: sampling,
                     childMapping: 'childs',
@@ -141,20 +137,43 @@
                     allowSorting: true,
                     searchSettings: { fields: ['orgcode', 'orgname']},
                     editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, showDeleteConfirmDialog: true,
-                        mode: 'Dialog', newRowPosition: 'Child', template:'#formModal' },
+                        mode: 'Row', newRowPosition: 'Child' },
                     treeColumnIndex: 1,
                     columns: [
                         { field: 'id', headerText: 'ID', isPrimaryKey: true, width: 45, textAlign: 'Center', visible: false},
-                        { field: 'orglevel', headerText: 'Org. Level', width: 120, textAlign: 'Left',
-                            allowSorting: false, type: 'number' },
                         { field: 'orgcode', headerText: 'Organization Code', width: 120, textAlign: 'Left'},
                         { field: 'orgparent', headerText: 'Organization Parent', width: 120, textAlign: 'Left', visible: false},
                         { field: 'orgname', headerText: 'Organization Name', width: 180, textAlign: 'Left'},
                         // { field: 'startDate', headerText: 'Start Date', width: 90, textAlign: 'Left', editType: 'datepickeredit', type: 'date', format: 'yMd', allowSorting: false },
-                        { field: 'status', headerText: 'Status', width: 80, textAlign: 'Left', allowSorting: false },
+                        { field: 'orglevel', headerText: 'Org. Level', width: 120, textAlign: 'Left',
+                            allowSorting: false, type: 'number' },
+                        { field: 'status', headerText: 'Status', width: 80, textAlign: 'Left', allowSorting: false,
+                            valueAccessor: getStatus, disableHtmlEncode: false },
                     ],
                     height: 270,
+                    actionBegin: function(args){
+                        if (args.requestType === 'save') {
+                            console.log('saved')
+                        }
+                    },
+                    actionComplete: function(args){
+                        if (args.requestType === 'beginEdit') {
+                            console.log(args.form.querySelector("#forgcode").value);
+                            // document.getElementById('osForm').attr('action', '/hr/ost/' + 1);
+                        }
+                        if (args.requestType === 'add') {
+                            console.log('add')
+                        }
+                    },
                 });
+
+                function getStatus(field, data, column){
+                    if (data.status == 1){
+                        return '<p class="text-success">Active</p>';
+                    } else if(data.status == 0){
+                        return '<p class="text-danger">Inactive</p>';
+                    }
+                }
 
                 treeGridObj.appendTo('#TreeGrid');
             </script>
