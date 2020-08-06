4d04120ac0c3395215b9e4cee6bcccdfbbd0ce3c
ibnuprtma <pibnu86@gmail.com>
2020-08-06 22:37:44 +0700

    modify UI modal user

--- Modules/Gate/Resources/views/components/user/modal.blade.php
+++ Modules/Gate/Resources/views/components/user/modal.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="userModal" role="dialog" aria-labelledby="editModalTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalTitle"></h5>
@@ -11,46 +11,47 @@
 
             <form method="POST" id="userForm">
                 <div class="modal-body">
-                    <div class="form-group row"><label class="col-sm-2 col-form-label">Username</label>
-                        <div class="col-sm-6">
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label>Username</label>
                             <input type="text" class="form-control " id="fusername" name="username" >
                             <div class="invalid-feedback-username text-danger"></div>
                         </div>
-                    </div>
-                    <div class="form-group row"><label class="col-sm-2 col-form-label">Name</label>
-                        <div class="col-sm-6">
+                        <div class="col-md-6">
+                            <label>Name</label>
                             <input type="text" class="form-control " id="fname" name="name" >
                             <div class="invalid-feedback-name text-danger"></div>
                         </div>
                     </div>
-                    <div class="form-group row"><label class="col-sm-2 col-form-label">Email</label>
-                        <div class="col-sm-6">
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label>Email</label>
                             <input type="email" class="form-control " id="femail" name="email" >
                             <div class="invalid-feedback-email text-danger"></div>
                         </div>
-                    </div>
-                    <div class="form-group row"><label class="col-sm-2 col-form-label">Password</label>
-                        <div class="col-sm-6">
+                        <div class="col-md-6">
+                            <label>Password</label>
                             <input type="password" class="form-control " id="fpassword" name="password">
                             <div class="invalid-feedback-password text-danger"></div>
                         </div>
                     </div>
-                    <div class="form-group row"><label class="col-sm-2 col-form-label">Company</label>
-                        <div class="col-sm-6">
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label>Company</label>
                             <select class="select2_company form-control m-b " id="fcompany" name="company">
                             </select>
                             <div class="invalid-feedback-company text-danger"></div>
                         </div>
-                    </div>
-                    <div class="form-group row"><label class="col-sm-2 col-form-label">Role</label>
-                        <div class="col-sm-6">
+                        <div class="col-md-6">
+                            <label>Role</label>
                             <select class="select2_role form-control m-b " id="frole" name="role">
                             </select>
                             <div class="invalid-feedback-role text-danger"></div>
                         </div>
                     </div>
-                    <div class="form-group row"><label class="col-sm-2 col-form-label">Status</label>
-                        <div class="col-sm-6">
+                    <div class="form-group row">
+                        <div class="col-md-6">
+                            <label>Status</label>
                             <select class="form-control m-b " id="fstatus" name="status">
                                 <option value="1">Active</option>
                                 <option value="0">Inactive</option>
@@ -64,7 +65,6 @@
                     <button class="btn btn-primary" type="submit" id="saveBtn"><strong>Save changes</strong></button>
                 </div>
             </form>
-
         </div>
     </div>
 </div>
