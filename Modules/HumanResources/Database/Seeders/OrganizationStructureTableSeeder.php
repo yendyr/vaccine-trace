a2833149174298317a66533dd926a23f968d5d00
satriyoaji <satriyoaji27@gmail.com>
2020-08-25 21:19:45 +0700

    remove seeder temporary

--- Modules/HumanResources/Database/Seeders/OrganizationStructureTableSeeder.php
+++ Modules/HumanResources/Database/Seeders/OrganizationStructureTableSeeder.php
@@ -18,50 +18,50 @@ class OrganizationStructureTableSeeder extends Seeder
     {
         Model::unguard();
 
-        $os = OrganizationStructure::create([
-            'orglevel' => '1',
-            'orgparent' => null,
-            'orgcode' => '0001',
-            'orgname' => 'Direksi',
-            'owned_by' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-        ]);
-        $os = OrganizationStructure::create([
-            'orglevel' => '2',
-            'orgparent' => '0001',
-            'orgcode' => '0002',
-            'orgname' => 'General Manager',
-            'owned_by' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-        ]);
-        $os = OrganizationStructure::create([
-            'orglevel' => '3',
-            'orgparent' => '0002',
-            'orgcode' => '0003',
-            'orgname' => 'Div. Fin & Acc',
-            'owned_by' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-        ]);
-        $os = OrganizationStructure::create([
-            'orglevel' => '2',
-            'orgparent' => '0001',
-            'orgcode' => '0004',
-            'orgname' => 'General HRD',
-            'owned_by' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-        ]);
-        $os = OrganizationStructure::create([
-            'orglevel' => '1',
-            'orgparent' => null,
-            'orgcode' => '0005',
-            'orgname' => 'Founder',
-            'owned_by' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-        ]);
+//        $os = OrganizationStructure::create([
+//            'orglevel' => '1',
+//            'orgparent' => null,
+//            'orgcode' => '0001',
+//            'orgname' => 'Direksi',
+//            'owned_by' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//        ]);
+//        $os = OrganizationStructure::create([
+//            'orglevel' => '2',
+//            'orgparent' => '0001',
+//            'orgcode' => '0002',
+//            'orgname' => 'General Manager',
+//            'owned_by' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//        ]);
+//        $os = OrganizationStructure::create([
+//            'orglevel' => '3',
+//            'orgparent' => '0002',
+//            'orgcode' => '0003',
+//            'orgname' => 'Div. Fin & Acc',
+//            'owned_by' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//        ]);
+//        $os = OrganizationStructure::create([
+//            'orglevel' => '2',
+//            'orgparent' => '0001',
+//            'orgcode' => '0004',
+//            'orgname' => 'General HRD',
+//            'owned_by' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//        ]);
+//        $os = OrganizationStructure::create([
+//            'orglevel' => '1',
+//            'orgparent' => null,
+//            'orgcode' => '0005',
+//            'orgname' => 'Founder',
+//            'owned_by' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//        ]);
     }
 }
