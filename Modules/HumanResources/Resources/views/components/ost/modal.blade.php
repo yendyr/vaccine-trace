0a570d7efc9665445682df60876c8c22940f76f7
satriyoaji <satriyoaji27@gmail.com>
2020-08-25 21:07:17 +0700

    modify fields ost

--- Modules/HumanResources/Resources/views/components/ost/modal.blade.php
+++ Modules/HumanResources/Resources/views/components/ost/modal.blade.php
@@ -18,7 +18,7 @@
                             <select class="select2_orgcode form-control m-b ml-2"  id="forgcode" name="orgcode"></select>
                             <div class="invalid-feedback-orgcode text-danger"></div>
                         </div>
-                        <div class="form-group col-sm-4">
+                        <div class="form-group col-sm-6">
                             <label class="col-form-label mr-2">Title Code</label>
                             <select class="form-control m-b " id="ftitlecode" name="titlecode">
                                 <option disabled selected>-- choose --</option>
@@ -33,7 +33,7 @@
                     </div>
 
                     <div class="row">
-                        <div class="form-group col-sm-6">
+                        <div class="form-group col-sm-8">
                             <label class="col-form-label mr-2">Job Title</label>
                             <input type="text" class="form-control" id="fjobtitle" name="jobtitle">
                             <div class="invalid-feedback-jobtitle text-danger"></div>
@@ -54,7 +54,7 @@
                             <select class="select2_rptorg form-control m-b " id="frptorg" name="rptorg"></select>
                             <div class="invalid-feedback-rptorg text-danger"></div>
                         </div>
-                        <div class="form-group col-sm-4">
+                        <div class="form-group col-sm-6">
                             <label class="col-form-label mr-2">Report Title</label>
                             <select class="select2_rpttitle form-control m-b " id="frpttitle" name="rpttitle"></select>
                             <div class="invalid-feedback-rpttitle text-danger"></div>
