783eee1f207d29085dcf19ffce589d89a0b52337
satriyoaji <satriyoaji27@gmail.com>
2020-09-07 21:22:29 +0700

    change date format

--- Modules/HumanResources/Database/Migrations/2020_09_04_075507_create_working_hours_table.php
+++ Modules/HumanResources/Database/Migrations/2020_09_04_075507_create_working_hours_table.php
@@ -17,15 +17,15 @@ class CreateWorkingHoursTable extends Migration
             $table->bigIncrements('id');
             $table->uuid('uuid')->unique();
             $table->string('empid', 20)->comment('Nomor NIK karyawan');
-            $table->date('workdate')->comment('Tanggal kehadiran  [format: Y/m/d]');
+            $table->date('workdate')->comment('Tanggal kehadiran  [format: Y-m-d]');
             $table->char('shiftno', 1)->comment('nomor shift');
-            $table->date('whdatestart')->comment('Tanggal masuk, [format: Y/m/d]')->nullable();
+            $table->date('whdatestart')->comment('Tanggal masuk, [format: Y-m-d]')->nullable();
             $table->time('whtimestart', 10)->comment('jam masuk, seperti 08:00:00')->nullable();
-            $table->date('whdatefinish')->comment('Tanggal pulang, [format: Y/m/d]')->nullable();
+            $table->date('whdatefinish')->comment('Tanggal pulang, [format: Y-m-d]')->nullable();
             $table->time('whtimefinish', 10)->comment('jam pulang, seperti 17:00:00')->nullable();
-            $table->date('rsdatestart')->comment('Tanggal mulai istirahat, [format: Y/m/d]')->nullable();
+            $table->date('rsdatestart')->comment('Tanggal mulai istirahat, [format: Y-m-d]')->nullable();
             $table->time('rstimestart', 10)->comment('jam mulai istirahat, seperti 11:30:00')->nullable();
-            $table->date('rsdatefinish')->comment('Tanggal selesai istirahat, [format: Y/m/d]')->nullable();
+            $table->date('rsdatefinish')->comment('Tanggal selesai istirahat, [format: Y-m-d]')->nullable();
             $table->time('rstimefinish', 10)->comment('jam berakhir istirahat, seperti 11:30:00')->nullable();
             $table->integer('stdhours')->comment('standard jam kerja (satuan jam)')->nullable();
             $table->integer('minhours')->comment('minimum jam kerja (satuan jam)')->nullable();
@@ -35,7 +35,7 @@ class CreateWorkingHoursTable extends Migration
             $table->double('leavehours')->comment('jumlah total jam ijin semetara/ijin potong jam kerja')->nullable();
             $table->double('attdhours')->comment('jumlah total jam kehadiran [jam kerja dan lembur (awal & akhir)]')->nullable();
             $table->double('overhours')->comment('jumlah total jam lembur lain')->nullable();
-            $table->string('attdstatus', 2)->comment('status kehadiran [ie: 01=hadir,02=mangkirm03=ijin]')->nullable();
+            $table->string('attdstatus', 2)->comment('status kehadiran [ie: 01=hadir,02=mangkir,03=ijin]')->nullable();
 
             $table->integer('status')->nullable();
             $table->integer('owned_by')->nullable()->unsigned();
--- Modules/HumanResources/Database/Migrations/2020_09_07_082309_create_working_hour_details_table.php
+++ Modules/HumanResources/Database/Migrations/2020_09_07_082309_create_working_hour_details_table.php
@@ -17,17 +17,17 @@ class CreateWorkingHourDetailsTable extends Migration
             $table->bigIncrements('id');
             $table->uuid('uuid')->unique();
             $table->string('empid', 20)->comment('Nomor NIK karyawan');
-            $table->date('workdate')->comment('Tanggal kehadiran  [format: Y/m/d]');
+            $table->date('workdate')->comment('Tanggal kehadiran  [format: Y-m-d]');
             $table->char('attdtype', 4)->comment('jenis kehadiran (i.e: 01=kerja, 02=Ijin semnetara')->nullable();
-            $table->date('datestart')->comment('Tanggal masuk, [format: Y/m/d]')->nullable();
+            $table->date('datestart')->comment('Tanggal masuk, [format: Y-m-d]')->nullable();
             $table->time('timestart')->comment('jam masuk, seperti 08:40:30')->nullable();
-            $table->date('datefinish')->comment('Tanggal pulang, [format: Y/m/d]')->nullable();
+            $table->date('datefinish')->comment('Tanggal pulang, [format: Y-m-d]')->nullable();
             $table->time('timefinish')->comment('jam pulang, seperti 17:00:00')->nullable();
             $table->dateTime('processedon')->comment('waktu proses kalkulasi/re   (result')->nullable();
             $table->string('mainattd', 1)->comment('Kehadiran jam kerja [Y=yes, N=no]')->nullable();
-            $table->date('caldatestart')->comment('Calculate tanggal mulai kehadiran, [format: Y/m/d]')->nullable();
+            $table->date('caldatestart')->comment('Calculate tanggal mulai kehadiran, [format: Y-m-d]')->nullable();
             $table->time('caltimestart')->comment('Calculate jam mulai kehadiran, [ie: 07:30:15]')->nullable();
-            $table->date('caldatefinish')->comment('Calculate tanggal brakhir kehadiran, [format: Y/m/d]')->nullable();
+            $table->date('caldatefinish')->comment('Calculate tanggal brakhir kehadiran, [format: Y-m-d]')->nullable();
             $table->time('caltimefinish')->comment('Calculate jam selesai kehadiran, [ie: 07:30:15]')->nullable();
 
             $table->integer('status')->nullable();
