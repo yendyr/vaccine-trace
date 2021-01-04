f8dbef54ac41d68ff4bcce5b8641bf7f7167ba9d
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-01-04 09:27:23 +0700

    Create Module PPC

---
+++ Modules/PPC/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('ppc::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('ppc.name') !!}
+    </p>
+@endsection
