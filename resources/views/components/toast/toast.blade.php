a3730be6453409669cfc4e24acdf4deb9dab9b01
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-01-09 22:53:30 +0700

    Updating Bootstrap & jQuery

--- resources/views/components/toast/toast.blade.php
+++ resources/views/components/toast/toast.blade.php
@@ -1,10 +1,10 @@
 <div style="position: absolute; top: 20px; right: 20px;">
-    <div class="toast notif animated slideInRight" role="alert" aria-live="assertive" aria-atomic="true" data-delay="6000" data-animation="true">
+    <div class="toast notif slideInRight" role="alert" aria-live="assertive" aria-atomic="true" data-delay="6000" data-animation="true">
         <div class="toast-header">
-            <i class="fa fa-info-circle"> </i>
+            <i class="fa fa-info-circle"></i>
             <strong class="mr-5 m-l-sm" id="notifTitle"></strong>
-            <button type="button" class="ml-5 mb-1 close" data-dismiss="toast" aria-label="Close">
-                <span aria-hidden="true" style="color: white">×</span>
+            <button type="button" class="ml-5 mb-1 close" data-dismiss="toast" aria-label="Close" style="position: absolute; right: 5px">
+                <span aria-hidden="true" style="color: white">x</span>
             </button>
         </div>
         <div class="toast-body" id="notifMessage"></div>
