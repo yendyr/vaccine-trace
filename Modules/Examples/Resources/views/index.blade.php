a298188726932b0cc35843b57f5edc87bab9b575
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-01-18 16:17:35 +0700

    Delete Examples Module

--- Modules/Examples/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('examples::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('examples.name') !!}
-    </p>
-@endsection
