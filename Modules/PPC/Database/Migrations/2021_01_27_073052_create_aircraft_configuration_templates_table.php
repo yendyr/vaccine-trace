47908997097c22f2608bbcdf11c15c5b1ef8df6b
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-01-27 14:55:18 +0700

    WIP Aircraft Configuration Template

---
+++ Modules/PPC/Database/Migrations/2021_01_27_073052_create_aircraft_configuration_templates_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAircraftConfigurationTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aircraft_configuration_templates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid')->unique();
+
+            $table->string('aircraft_type_id');
+            $table->string('code');
+            $table->string('name');
+            $table->string('description')->nullable();
+
+            $table->rememberToken();
+            $table->integer('status')->nullable();
+            $table->integer('owned_by')->nullable()->unsigned();
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aircraft_configuration_templates');
+    }
+}
