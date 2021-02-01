80bb89a0c413fffaee476bcb1c2cc18215b49cd4
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-02-01 11:25:55 +0700

    Create Some Seeder

--- Modules/SupplyChain/Database/Seeders/ItemCategorySeeder.php
+++ Modules/SupplyChain/Database/Seeders/ItemCategorySeeder.php
@@ -21,11 +21,21 @@ class ItemCategorySeeder extends Seeder
         ItemCategory::create([
             'name' => 'Component',
             'code' => 'CMP',
+            'sales_coa_id' => '1',
+            'inventory_coa_id' => '3',
+            'cost_coa_id' => '5',
+            'inventory_adjustment_coa_id' => '7',
+            'status' => '1',
             'uuid' => Str::uuid(),
         ]);
         ItemCategory::create([
             'name' => 'Raw Material',
             'code' => 'RW',
+            'sales_coa_id' => '1',
+            'inventory_coa_id' => '3',
+            'cost_coa_id' => '5',
+            'inventory_adjustment_coa_id' => '7',
+            'status' => '1',
             'uuid' => Str::uuid(),
         ]);
         ItemCategory::create([
