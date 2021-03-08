131e16e65ba578fd390814ef9156b67d26d03cb7
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-03-08 14:46:30 +0700

    WIP Stock Mutation Detail

---
+++ Modules/SupplyChain/Database/Migrations/2021_02_13_191816_create_stock_mutation_detail_initial_agings_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockMutationDetailInitialAgingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_mutation_detail_inital_agings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid')->unique();
+
+            $table->string('mutation_detail_id');
+
+            $table->date('expired_date')->nullable();
+            $table->double('initial_flight_hour')->nullable()->default(0);
+            $table->double('initial_block_hour')->nullable()->default(0);
+            $table->integer('initial_flight_cycle')->nullable()->default(0);
+            $table->integer('initial_flight_event')->nullable()->default(0);
+            $table->date('initial_start_date')->nullable();
+
+            $table->string('description')->nullable();
+            
+            $table->rememberToken();
+            $table->integer('status')->nullable();
+            $table->integer('owned_by')->nullable()->unsigned();
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_mutation_detail_inital_agings');
+    }
+}
--- Modules/SupplyChain/Entities/StockMutationDetailInitialAging.php
+++ Modules/SupplyChain/Entities/StockMutationDetailInitialAging.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modules\PPC\Entities;
+namespace Modules\SupplyChain\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
