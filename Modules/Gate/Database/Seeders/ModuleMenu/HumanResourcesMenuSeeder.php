2fb6b336366deea74c2db35e8311101cedcb77d9
satriyoaji27 <satriyoaji27@gmail.com>
2021-03-17 06:28:19 +0700

    remove unnecessary menu seeder

--- Modules/Gate/Database/Seeders/ModuleMenu/HumanResourcesMenuSeeder.php
+++ Modules/Gate/Database/Seeders/ModuleMenu/HumanResourcesMenuSeeder.php
@@ -57,25 +57,25 @@ class HumanResourcesMenuSeeder extends Seeder
             'parent_id' => $menuHrSetting->id
         ]);
         
-        Menu::create([
-            'menu_link' => 'hr/org-structure-title',
-            'menu_text' => 'Organization Structure Title',
-            'menu_route' => 'hr.user.index',
-            'menu_icon' => 'fa-user-circle-o',
-            'menu_class' => 'Modules\HumanResources\Entities\User',
-            'menu_id' => null,
-            'menu_actives' => json_encode(['hr/org-structure-title', 'hr/org-structure-title/*']),
-            'group' => 'Human Resources',
-            'add' => 1,
-            'update' => 1,
-            'delete' => 0,
-            'print' => 0,
-            'approval' => 0,
-            'process' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-            'parent_id' => null
-        ]);
+//        Menu::create([
+//            'menu_link' => 'hr/org-structure-title',
+//            'menu_text' => 'Organization Structure Title',
+//            'menu_route' => 'hr.user.index',
+//            'menu_icon' => 'fa-user-circle-o',
+//            'menu_class' => 'Modules\HumanResources\Entities\User',
+//            'menu_id' => null,
+//            'menu_actives' => json_encode(['hr/org-structure-title', 'hr/org-structure-title/*']),
+//            'group' => 'Human Resources',
+//            'add' => 1,
+//            'update' => 1,
+//            'delete' => 0,
+//            'print' => 0,
+//            'approval' => 0,
+//            'process' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//            'parent_id' => null
+//        ]);
         
         $workGroupMenu = Menu::create([
             'menu_link' => 'hr/working-group',
@@ -97,25 +97,25 @@ class HumanResourcesMenuSeeder extends Seeder
             'parent_id' => $menuHrSetting->id
         ]);
         
-        Menu::create([
-            'menu_link' => 'hr/working-group-detail',
-            'menu_text' => 'Working Group Detail',
-            'menu_route' => 'hr.workgroup-detail.index',
-            'menu_icon' => 'fa-plus',
-            'menu_class' => 'Modules\HumanResources\Entities\WorkingGroupDetail',
-            'menu_id' => null,
-            'menu_actives' => json_encode(['hr/working-group-detail', 'hr/working-group-detail/*']),
-            'group' => 'Human Resources',
-            'add' => 1,
-            'update' => 1,
-            'delete' => 0,
-            'print' => 0,
-            'approval' => 0,
-            'process' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-            'parent_id' => $workGroupMenu->id
-        ]);
+//        Menu::create([
+//            'menu_link' => 'hr/working-group-detail',
+//            'menu_text' => 'Working Group Detail',
+//            'menu_route' => 'hr.workgroup-detail.index',
+//            'menu_icon' => 'fa-plus',
+//            'menu_class' => 'Modules\HumanResources\Entities\WorkingGroupDetail',
+//            'menu_id' => null,
+//            'menu_actives' => json_encode(['hr/working-group-detail', 'hr/working-group-detail/*']),
+//            'group' => 'Human Resources',
+//            'add' => 1,
+//            'update' => 1,
+//            'delete' => 0,
+//            'print' => 0,
+//            'approval' => 0,
+//            'process' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//            'parent_id' => $workGroupMenu->id
+//        ]);
         
         Menu::create([
             'menu_link' => 'hr/holiday',
@@ -157,85 +157,85 @@ class HumanResourcesMenuSeeder extends Seeder
             'parent_id' => null
         ]);
         
-        Menu::create([
-            'menu_link' => 'hr/id-card',
-            'menu_text' => 'ID Card',
-            'menu_route' => 'hr.id-card.index',
-            'menu_icon' => 'fa-plus',
-            'menu_class' => 'Modules\HumanResources\Entities\idCard',
-            'menu_id' => null,
-            'menu_actives' => json_encode(['hr/id-card', 'hr/id-card/*']),
-            'group' => 'Human Resources',
-            'add' => 1,
-            'update' => 1,
-            'delete' => 0,
-            'print' => 0,
-            'approval' => 0,
-            'process' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-            'parent_id' => null
-        ]);
+//        Menu::create([
+//            'menu_link' => 'hr/id-card',
+//            'menu_text' => 'ID Card',
+//            'menu_route' => 'hr.id-card.index',
+//            'menu_icon' => 'fa-plus',
+//            'menu_class' => 'Modules\HumanResources\Entities\idCard',
+//            'menu_id' => null,
+//            'menu_actives' => json_encode(['hr/id-card', 'hr/id-card/*']),
+//            'group' => 'Human Resources',
+//            'add' => 1,
+//            'update' => 1,
+//            'delete' => 0,
+//            'print' => 0,
+//            'approval' => 0,
+//            'process' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//            'parent_id' => null
+//        ]);
         
-        Menu::create([
-            'menu_link' => 'hr/education',
-            'menu_text' => 'Education',
-            'menu_route' => 'hr.education.index',
-            'menu_icon' => 'fa-plus',
-            'menu_class' => 'Modules\HumanResources\Entities\Education',
-            'menu_id' => null,
-            'menu_actives' => json_encode(['hr/education', 'hr/education/*']),
-            'group' => 'Human Resources',
-            'add' => 1,
-            'update' => 1,
-            'delete' => 0,
-            'print' => 0,
-            'approval' => 0,
-            'process' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-            'parent_id' => null
-        ]);
+//        Menu::create([
+//            'menu_link' => 'hr/education',
+//            'menu_text' => 'Education',
+//            'menu_route' => 'hr.education.index',
+//            'menu_icon' => 'fa-plus',
+//            'menu_class' => 'Modules\HumanResources\Entities\Education',
+//            'menu_id' => null,
+//            'menu_actives' => json_encode(['hr/education', 'hr/education/*']),
+//            'group' => 'Human Resources',
+//            'add' => 1,
+//            'update' => 1,
+//            'delete' => 0,
+//            'print' => 0,
+//            'approval' => 0,
+//            'process' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//            'parent_id' => null
+//        ]);
         
-        Menu::create([
-            'menu_link' => 'hr/family',
-            'menu_text' => 'Family',
-            'menu_route' => 'hr.family.index',
-            'menu_icon' => 'fa-plus',
-            'menu_class' => 'Modules\HumanResources\Entities\Family',
-            'menu_id' => null,
-            'menu_actives' => json_encode(['hr/family', 'hr/family/*']),
-            'group' => 'Human Resources',
-            'add' => 1,
-            'update' => 1,
-            'delete' => 0,
-            'print' => 0,
-            'approval' => 0,
-            'process' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-            'parent_id' => null
-        ]);
+//        Menu::create([
+//            'menu_link' => 'hr/family',
+//            'menu_text' => 'Family',
+//            'menu_route' => 'hr.family.index',
+//            'menu_icon' => 'fa-plus',
+//            'menu_class' => 'Modules\HumanResources\Entities\Family',
+//            'menu_id' => null,
+//            'menu_actives' => json_encode(['hr/family', 'hr/family/*']),
+//            'group' => 'Human Resources',
+//            'add' => 1,
+//            'update' => 1,
+//            'delete' => 0,
+//            'print' => 0,
+//            'approval' => 0,
+//            'process' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//            'parent_id' => null
+//        ]);
         
-        Menu::create([
-            'menu_link' => 'hr/address',
-            'menu_text' => 'Address',
-            'menu_route' => 'hr.address.index',
-            'menu_icon' => 'fa-plus',
-            'menu_class' => 'Modules\HumanResources\Entities\Address',
-            'menu_id' => null,
-            'menu_actives' => json_encode(['hr/address', 'hr/address/*']),
-            'group' => 'Human Resources',
-            'add' => 1,
-            'update' => 1,
-            'delete' => 0,
-            'print' => 0,
-            'approval' => 0,
-            'process' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-            'parent_id' => null
-        ]);
+//        Menu::create([
+//            'menu_link' => 'hr/address',
+//            'menu_text' => 'Address',
+//            'menu_route' => 'hr.address.index',
+//            'menu_icon' => 'fa-plus',
+//            'menu_class' => 'Modules\HumanResources\Entities\Address',
+//            'menu_id' => null,
+//            'menu_actives' => json_encode(['hr/address', 'hr/address/*']),
+//            'group' => 'Human Resources',
+//            'add' => 1,
+//            'update' => 1,
+//            'delete' => 0,
+//            'print' => 0,
+//            'approval' => 0,
+//            'process' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//            'parent_id' => null
+//        ]);
         
         Menu::create([
             'menu_link' => 'hr/working-hour',
@@ -257,45 +257,45 @@ class HumanResourcesMenuSeeder extends Seeder
             'parent_id' => null
         ]);
         
-        Menu::create([
-            'menu_link' => 'hr/working-hour-detail',
-            'menu_text' => 'Working Hour detail',
-            'menu_route' => 'hr.working-hour-detail.index',
-            'menu_icon' => 'fa-plus',
-            'menu_class' => 'Modules\HumanResources\Entities\WorkingHourDetail',
-            'menu_id' => null,
-            'menu_actives' => json_encode(['hr/working-hour-detail', 'hr/working-hour-detail/*']),
-            'group' => 'Human Resources',
-            'add' => 1,
-            'update' => 1,
-            'delete' => 0,
-            'print' => 0,
-            'approval' => 0,
-            'process' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-            'parent_id' => null
-        ]);
+//        Menu::create([
+//            'menu_link' => 'hr/working-hour-detail',
+//            'menu_text' => 'Working Hour detail',
+//            'menu_route' => 'hr.working-hour-detail.index',
+//            'menu_icon' => 'fa-plus',
+//            'menu_class' => 'Modules\HumanResources\Entities\WorkingHourDetail',
+//            'menu_id' => null,
+//            'menu_actives' => json_encode(['hr/working-hour-detail', 'hr/working-hour-detail/*']),
+//            'group' => 'Human Resources',
+//            'add' => 1,
+//            'update' => 1,
+//            'delete' => 0,
+//            'print' => 0,
+//            'approval' => 0,
+//            'process' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//            'parent_id' => null
+//        ]);
         
-        Menu::create([
-            'menu_link' => 'hr/working-hour-attendance',
-            'menu_text' => 'Working Hour attendance',
-            'menu_route' => 'hr.working-hour-attendance.index',
-            'menu_icon' => 'fa-plus',
-            'menu_class' => 'Modules\HumanResources\Entities\WorkingHourAttendance',
-            'menu_id' => null,
-            'menu_actives' => json_encode(['hr/working-hour-attendance', 'hr/working-hour-attendance/*']),
-            'group' => 'Human Resources',
-            'add' => 1,
-            'update' => 1,
-            'delete' => 0,
-            'print' => 0,
-            'approval' => 0,
-            'process' => 0,
-            'status' => 1,
-            'uuid' => Str::uuid(),
-            'parent_id' => null
-        ]);
+//        Menu::create([
+//            'menu_link' => 'hr/working-hour-attendance',
+//            'menu_text' => 'Working Hour attendance',
+//            'menu_route' => 'hr.working-hour-attendance.index',
+//            'menu_icon' => 'fa-plus',
+//            'menu_class' => 'Modules\HumanResources\Entities\WorkingHourAttendance',
+//            'menu_id' => null,
+//            'menu_actives' => json_encode(['hr/working-hour-attendance', 'hr/working-hour-attendance/*']),
+//            'group' => 'Human Resources',
+//            'add' => 1,
+//            'update' => 1,
+//            'delete' => 0,
+//            'print' => 0,
+//            'approval' => 0,
+//            'process' => 0,
+//            'status' => 1,
+//            'uuid' => Str::uuid(),
+//            'parent_id' => null
+//        ]);
         
         Menu::create([
             'menu_link' => 'hr/attendance',
