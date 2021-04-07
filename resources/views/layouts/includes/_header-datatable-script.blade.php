09bcc86751ebcacf836c97dcbe80fba8f200e2cb
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-04-07 16:15:47 +0700

    Add Scroller Plugin to Datatables

--- resources/views/layouts/includes/_header-datatable-script.blade.php
+++ resources/views/layouts/includes/_header-datatable-script.blade.php
@@ -1,3 +1,3 @@
 <!-- Data Table, USE THIS ONLY IN SPECIFIC FORM -->
-<link href="{{URL::asset('theme/css/plugins/dataTables/datatables.min.css')}}" rel="stylesheet">
-{{-- <link href="{{URL::asset('theme/css/plugins/dataTables/dataTables.bootstrap4.min.css')}}" rel="stylesheet"> --}}
+{{-- <link href="{{URL::asset('theme/css/plugins/dataTables/datatables.min.css')}}" rel="stylesheet"> --}}
+<link href="{{URL::asset('theme/css/plugins/dataTables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
