e21d5452ca05d8b0f4f4ae0a8acb180bbb5b7e30
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-04-24 21:36:01 +0700

    Solve Edited JS by Husni

--- Modules/PPC/Resources/views/components/maintenance-program/taskcard-list/_script.blade.php
+++ Modules/PPC/Resources/views/components/maintenance-program/taskcard-list/_script.blade.php
@@ -163,7 +163,7 @@ $(document).ready(function () {
         serverSide: false,
         searchDelay: 1500,
         ajax: {
-            url: "/ppc/maintenance-program-detail/?maintenance_program_id=" + "{{ null }}",
+            url: "/ppc/maintenance-program-detail/?maintenance_program_id=" + "{{ $MaintenanceProgram->id }}",
         },
         columns: [
             { data: 'taskcard.mpd_number', 
