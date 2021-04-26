5c3ace0120ddd53befd525b32ffd456c125bb433
M.Husni Thamrin <husni.sosnet@gmail.com>
2021-04-26 14:14:10 +0700

    create controller and register on web route [PPC}

---
+++ Modules/PPC/Http/Controllers/WOWPTaskcardItemController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Modules\PPC\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Modules\PPC\Entities\Taskcard;
+use Modules\PPC\Entities\WorkOrder;
+use Modules\PPC\Entities\WorkOrderWorkPackage;
+use Modules\PPC\Entities\WOWPTaskcardItem;
+
+class WOWPTaskcardItemController extends Controller
+{
+    use AuthorizesRequests;
+
+    public function __construct()
+    {
+        $this->authorizeResource(WorkOrder::class);
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request, WorkOrder $work_order, WorkOrderWorkPackage $work_package)
+    {
+        if ($request->ajax()) {
+            $data = WOWPTaskcardItem::query()
+                                        ->where()
+                                        ->with([
+                                            'item:id,code,name',
+                                            'unit:id,name',
+                                            'category:id,name',
+                                        ]);
+                                        
+            return Datatables::of($data)  
+                ->addColumn('creator_name', function($row){
+                    return $row->creator->name ?? '-';
+                })
+                ->addColumn('updater_name', function($row){
+                    return $row->updater->name ?? '-';
+                })
+                ->addColumn('action', function($row){
+                    $noAuthorize = true;
+                    if(Auth::user()->can('update', Taskcard::class)) {
+                        $updateable = 'button';
+                        $updateValue = $row->id;
+                        $editButtonClass = 'editButtonItem';
+                        $noAuthorize = false;
+                    }
+                    if(Auth::user()->can('delete', Taskcard::class)) {
+                        $deleteable = true;
+                        $deleteButtonClass = 'deleteButtonItem';
+                        $deleteId = $row->id;
+                        $noAuthorize = false;
+                    }
+
+                    if ($noAuthorize == false) {
+                        return view('components.action-button', compact(['updateable', 'updateValue','editButtonClass','deleteable', 'deleteId', 'deleteButtonClass']));
+                    }
+                    else {
+                        return '<p class="text-muted font-italic">Not Authorized</p>';
+                    }
+                    
+                })
+                ->escapeColumns([])
+                ->make(true);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Renderable
+     */
+    public function create(Request $request, WorkOrder $work_order, WorkOrderWorkPackage $work_package, Taskcard $taskcard)
+    {
+        return view('ppc::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Renderable
+     */
+    public function store(Request $request, WorkOrder $work_order, WorkOrderWorkPackage $work_package, Taskcard $taskcard, WOWPTaskcardItem $item)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show(Request $request, WorkOrder $work_order, WorkOrderWorkPackage $work_package, Taskcard $taskcard, WOWPTaskcardItem $item)
+    {
+        return view('ppc::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function edit(Request $request, WorkOrder $work_order, WorkOrderWorkPackage $work_package, Taskcard $taskcard, WOWPTaskcardItem $item)
+    {
+        return view('ppc::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Renderable
+     */
+    public function update(Request $request, WorkOrder $work_order, WorkOrderWorkPackage $work_package, Taskcard $taskcard, WOWPTaskcardItem $item)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy(Request $request, WorkOrder $work_order, WorkOrderWorkPackage $work_package, Taskcard $taskcard, WOWPTaskcardItem $item)
+    {
+        //
+    }
+}
--- Modules/PPC/Routes/web.php
+++ Modules/PPC/Routes/web.php
@@ -11,55 +11,55 @@
 |
 */
 Route::name('ppc.')->group(function () {
-    Route::prefix('ppc')->group(function() {
+    Route::prefix('ppc')->group(function () {
         Route::resource('/taskcard-type', 'TaskcardTypeController');
-        Route::name('taskcard-type.')->group(function() {
+        Route::name('taskcard-type.')->group(function () {
             Route::get('ppc/taskcard-type/select2', 'TaskcardTypeController@select2')->name('select2');
         });
 
         Route::get('/taskcard-group/detail-tree', 'TaskcardGroupController@tree')->name('taskcard-group.detail-tree');
 
         Route::resource('/taskcard-group', 'TaskcardGroupController');
-        Route::name('taskcard-group.')->group(function() {
+        Route::name('taskcard-group.')->group(function () {
             Route::get('ppc/taskcard-group/select2/parent', 'TaskcardGroupController@select2Parent')->name('select2.parent');
             Route::get('ppc/taskcard-group/select2/child', 'TaskcardGroupController@select2Child')->name('select2.child');
         });
 
         Route::resource('/taskcard-tag', 'TaskcardTagController');
-        Route::name('taskcard-tag.')->group(function() {
+        Route::name('taskcard-tag.')->group(function () {
             Route::get('ppc/taskcard-tag/select2/parent', 'TaskcardTagController@select2')->name('select2');
         });
 
         Route::resource('/taskcard-workarea', 'TaskcardWorkareaController');
-        Route::name('taskcard-workarea.')->group(function() {
+        Route::name('taskcard-workarea.')->group(function () {
             Route::get('ppc/taskcard-workarea/select2', 'TaskcardWorkareaController@select2')->name('select2');
         });
 
         Route::resource('/taskcard-access', 'TaskcardAccessController');
-        Route::name('taskcard-access.')->group(function() {
+        Route::name('taskcard-access.')->group(function () {
             Route::get('ppc/taskcard-access/select2', 'TaskcardAccessController@select2')->name('select2');
         });
 
         Route::resource('/taskcard-zone', 'TaskcardZoneController');
-        Route::name('taskcard-zone.')->group(function() {
+        Route::name('taskcard-zone.')->group(function () {
             Route::get('ppc/taskcard-zone/select2', 'TaskcardZoneController@select2')->name('select2');
         });
 
         Route::resource('/taskcard-document-library', 'TaskcardDocumentLibraryController');
-        Route::name('taskcard-document-library.')->group(function() {
+        Route::name('taskcard-document-library.')->group(function () {
             Route::get('ppc/taskcard-document-library/select2', 'TaskcardDocumentLibraryController@select2')->name('select2');
         });
-        
+
         Route::resource('/aircraft-type', 'AircraftTypeController');
-        Route::name('aircraft-type.')->group(function() {
+        Route::name('aircraft-type.')->group(function () {
             Route::get('ppc/aircraft-type/select2', 'AircraftTypeController@select2')->name('select2');
         });
 
-        Route::name('taskcard.')->group(function() {
+        Route::name('taskcard.')->group(function () {
             Route::patch('/taskcard/update-control-parameter/{taskcard}', 'TaskcardController@updateControlParameter')->name('updateControlParameter');
         });
         Route::resource('/taskcard', 'TaskcardController');
-        Route::name('taskcard.')->group(function() {
+        Route::name('taskcard.')->group(function () {
             Route::post('/taskcard/file-upload/{taskcard}', 'TaskcardController@fileUpload')->name('file-upload');
         });
 
@@ -68,7 +68,7 @@ Route::name('ppc.')->group(function () {
         Route::resource('/taskcard-detail-item', 'TaskcardDetailItemController');
 
         Route::resource('/maintenance-program', 'MaintenanceProgramController');
-        Route::name('maintenance-program.')->group(function() {
+        Route::name('maintenance-program.')->group(function () {
             Route::get('ppc/maintenance-program/select2', 'MaintenanceProgramController@select2')->name('select2');
         });
 
@@ -77,39 +77,52 @@ Route::name('ppc.')->group(function () {
         Route::get('/aircraft-configuration-template/detail-tree', 'AircraftConfigurationTemplateDetailController@tree')->name('aircraft-configuration-template.detail-tree');
 
         Route::resource('/aircraft-configuration-template', 'AircraftConfigurationTemplateController');
-        Route::name('aircraft-configuration-template.')->group(function() {
+        Route::name('aircraft-configuration-template.')->group(function () {
             Route::get('ppc/aircraft-configuration-template/select2', 'AircraftConfigurationTemplateController@select2')->name('select2');
         });
 
-        Route::resource('/configuration-template-detail', 'AircraftConfigurationTemplateDetailController');        
-        Route::name('configuration-template-detail.')->group(function() {
+        Route::resource('/configuration-template-detail', 'AircraftConfigurationTemplateDetailController');
+        Route::name('configuration-template-detail.')->group(function () {
             Route::get('ppc/configuration-template-detail/select2', 'AircraftConfigurationTemplateDetailController@select2Parent')->name('select2');
         });
 
         Route::get('/aircraft-configuration/detail-tree', 'AircraftConfigurationDetailController@tree')->name('aircraft-configuration.detail-tree');
 
         Route::resource('/aircraft-configuration', 'AircraftConfigurationController');
-        Route::name('aircraft-configuration.')->group(function() {
+        Route::name('aircraft-configuration.')->group(function () {
             Route::get('ppc/aircraft-configuration/select2', 'AircraftConfigurationController@select2')->name('select2');
         });
         Route::post('/aircraft-configuration/{aircraft_configuration}/approve', 'AircraftConfigurationController@approve');
 
-        Route::resource('/configuration-detail', 'AircraftConfigurationDetailController');        
-        Route::name('configuration-detail.')->group(function() {
+        Route::resource('/configuration-detail', 'AircraftConfigurationDetailController');
+        Route::name('configuration-detail.')->group(function () {
             Route::get('ppc/configuration-detail/select2', 'AircraftConfigurationDetailController@select2Parent')->name('select2');
         });
 
         Route::resource('/item-aging-report', 'ItemStockAgingController');
-        
-        Route::resource('/aircraft-aging-report', 'AircraftAgingController');   
-        
-        Route::name('work-order.')->prefix('work-order')->group(function() {
+
+        Route::resource('/aircraft-aging-report', 'AircraftAgingController');
+
+        Route::name('work-order.')->prefix('work-order')->group(function () {
+
             Route::get('/select2/aircraft', 'WorkOrderController@select2Aircraft')->name('select2.aircraft');
-            Route::name('work-package.')->prefix('{work_order}/work-package')->group(function() {
-                Route::resource('{work_package}/taskcard', 'WorkOrderWorkPackageTaskcardController');
+
+            Route::name('work-package.')->prefix('{work_order}/work-package/{work_package}')->group(function () {
+
+                Route::get('/item', 'WOWPTaskcardItemController@index')->name('item.index');
+
+                Route::name('taskcard.')->prefix('taskcard/{taskcard}')->group(function () {
+                    Route::resource('item', 'WOWPTaskcardItemController')->except(['index']);
+                });
+
+                Route::resource('taskcard', 'WorkOrderWorkPackageTaskcardController');
+
             });
+
             Route::resource('{work_order}/work-package', 'WorkOrderWorkPackageController');
+
         });
+
         Route::resource('/work-order', 'WorkOrderController');
     });
-}); 
+});
