8ddad3f0c852791198f195101bbb12275865256a
ThamrinPH <husni.sosnet@gmail.com>
2021-05-05 14:48:21 +0700

    create new entity taskcard/jobcard progress detail

---
+++ Modules/PPC/Database/Migrations/2021_05_05_142329_create_w_o_w_p_taskcard_progress_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWOWPTaskcardProgressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('w_o_w_p_taskcard_progress', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid')->unique();
+
+            $table->string('work_order_id')->nullable();
+            $table->string('work_package_id')->nullable();
+            $table->string('taskcard_id')->nullable();
+            
+            $table->string('transaction_status')->nullable();
+            $table->string('progress_notes')->nullable();
+
+            $table->integer('status')->nullable();
+            $table->integer('owned_by')->nullable()->unsigned();
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('w_o_w_p_taskcard_progress');
+    }
+}
