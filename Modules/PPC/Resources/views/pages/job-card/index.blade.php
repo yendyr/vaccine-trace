d587f8a39a88e2dd93272ed1f2c1b14a371d25a2
M.Husni Thamrin <husni.sosnet@gmail.com>
2021-05-11 15:14:09 +0700

    change scan job card page

--- Modules/PPC/Resources/views/pages/job-card/index.blade.php
+++ Modules/PPC/Resources/views/pages/job-card/index.blade.php
@@ -1,29 +1,45 @@
 @extends('layouts.master')
 
 @section('content')
-    @include('ppc::pages.job-card.modal')
 
-    @component('components.crud-form.index',[
-                    'title' => 'Job Card Datalist',
-                    'tableId' => 'job-card-table'])
-    @endcomponent
+<div class="passwordBox animated fadeInDown">
+    <div class="row">
 
-    @include('ppc::components.job-card._script')
+        <div class="col-md-12">
+            <div class="ibox-content">
+
+                <h2 class="font-bold text-center">Execute Job Card</h2>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <form class="m-t" role="inputForm" action="#">
+                            <div class="form-group">
+                                <input type="password" class="form-control" placeholder="Scan QR Code here" required="" autofocus>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary block full-width m-b hidden">Execute</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr />
+</div>
+
 
 @endsection
 
 @push('header-scripts')
-    @include('layouts.includes._header-datatable-script')
-    <style>
-        thead input {
-            width: 100%;
-        }
-        tr.group,
-        tr.group:hover {
-            background-color: #aaa !important;
-        }
-    </style>
-@endpush
-@push('footer-scripts')
-    @include('layouts.includes._footer-datatable-script')
+@include('layouts.includes._header-datatable-script')
+<style>
+    thead input {
+        width: 100%;
+    }
+
+    tr.group,
+    tr.group:hover {
+        background-color: #aaa !important;
+    }
+</style>
 @endpush
