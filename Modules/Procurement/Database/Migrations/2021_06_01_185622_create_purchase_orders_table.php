1999971e07ad584043c6d52a611cf7fd35baf1a3
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-06-01 20:21:45 +0700

    WIP: Purchase Order

--- Modules/Procurement/Database/Migrations/2021_06_01_185622_create_purchase_orders_table.php
+++ Modules/Procurement/Database/Migrations/2021_06_01_185622_create_purchase_orders_table.php
@@ -18,12 +18,21 @@ class CreatePurchaseOrdersTable extends Migration
             $table->uuid('uuid')->unique();
 
             $table->string('code')->nullable();
-            $table->date('transaction_date')->nullable();
+            $table->date('transaction_date');
+            $table->date('valid_until_date')->nullable();
+            $table->date('shipping_date')->nullable();
+            $table->string('shipping_address')->nullable();
+            $table->string('supplier_id');
             $table->string('description')->nullable();
+            $table->string('term_and_condition')->nullable();
+
+            $table->string('current_primary_currency_id');
+            $table->string('currency_id');
+            $table->double('exchange_rate')->default(1);
 
             $table->string('transaction_reference_id')->nullable();
-            $table->string('transaction_reference_class')->nullable();
-            $table->string('transaction_reference_text')->nullable();
+            $table->string('transaction_reference_class')->default('\Modules\Procurement\Entities\PurchaseRequisition');
+            $table->string('transaction_reference_text')->default('Purchase Requisition');
             $table->string('transaction_reference_url')->nullable();
 
             $table->rememberToken();
