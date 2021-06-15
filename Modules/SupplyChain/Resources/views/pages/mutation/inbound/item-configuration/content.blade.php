a327e090c0c9a5ae7062220e27585a73e41f3c98
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-06-15 16:55:28 +0700

    QR Code for Each Item Stocks

--- Modules/SupplyChain/Resources/views/pages/mutation/inbound/item-configuration/content.blade.php
+++ Modules/SupplyChain/Resources/views/pages/mutation/inbound/item-configuration/content.blade.php
@@ -38,7 +38,11 @@
         {{-- <th>Print QR</th> --}}
         {{-- <th>Created By</th> --}}
         <th>Created At</th>
-        <th>Action</th>
+        @if($MutationInbound->approvals()->count() == 0)
+            <th>Action</th>
+        @else
+            <th>Print QR Code</th>
+        @endif
     @endslot
     @endcomponent
 </div>
