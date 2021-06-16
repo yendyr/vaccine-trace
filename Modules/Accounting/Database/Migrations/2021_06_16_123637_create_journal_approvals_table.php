f70018bbb21e429bd99ae295242801e636c8d2f0
Rachmatulloh Yendy <yendy_rachmatulloh@yahoo.com>
2021-06-16 12:55:42 +0700

    WIP: Journal

---
+++ Modules/Accounting/Database/Migrations/2021_06_16_123637_create_journal_approvals_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJournalApprovalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journal_approvals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid')->unique();
+
+            $table->string('purchase_order_id');
+            $table->string('approval_notes');
+
+            $table->rememberToken();
+            $table->integer('status')->nullable();
+            $table->integer('owned_by')->nullable()->unsigned();
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journal_approvals');
+    }
+}
